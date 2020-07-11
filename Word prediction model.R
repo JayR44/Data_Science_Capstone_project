9cd117a84baa73977435c4a414dabfb5f4ff8a6a
Julia R <julia.watkinson@barnett-waddingham.co.uk>
2020-07-11 10:13:27 +0100

    files generated

---
+++ Word prediction model.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(quanteda)
+library(tm)
+
+file1 <- readRDS("./file1.RDS")
+file2 <- readRDS("./file2.RDS")
+combined <- c(file1, file2)
+
+#Use sample to regenerate code
+vec_cor_1000 <- readRDS("./DATA/US_corpus_vec.RDS")
+df <- dataframe <- data.frame(text=unlist(sapply(vec_cor_1000, `[`, "content")), 
+                              stringsAsFactors=F)
+file_1000_to_use <- df[[1]]
+saveRDS(file_1000_to_use, "./sample_1000_to_use.RDS")
