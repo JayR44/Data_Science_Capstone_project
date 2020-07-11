6c27794cf46a2bb32baf416fd7d9896e713c437b
Julia R <julia.watkinson@barnett-waddingham.co.uk>
2020-07-11 12:15:16 +0100

    Predict model

--- Functions.R
+++ Functions.R
@@ -19,3 +19,47 @@ clean_text <- function(text){
     stripWhitespace() %>%
     strip()
 }
+
+predict_word <- function(ngram_table, phrase){
+  
+  #Given a sentence, the model will predict the next word
+  
+  #Obtain lines from the data table with the last few words of ngram_1 matching the phrase
+  sub_lines <- ngram_table[grep(paste0(" ", phrase,"$"), ngram_1)]
+  
+  loop <- 1
+  len_phrase <- wordcount(phrase);
+  
+  #If there are no matches
+  while(NROW(sub_lines) == 0) {
+    
+    #Remove the last word from the phrase
+    phrase <- str_replace(phrase, paste0("^", word(phrase,1), " "), "")
+    sub_lines <- ngram_table[grep(paste0(" ", phrase,"$"), ngram_1)]
+    
+    loop <- loop + 1
+    
+    #Stop if the loop equals the length of the phrase
+    if(loop == len_phrase){
+      break
+    }
+    
+  }
+  
+  if(NROW(sub_lines) == 0){
+    
+    stop("No prediction found")
+    
+  }
+  
+  options <- sub_lines %>%
+    group_by(pred) %>%   #group_by(pred) %>%    whould show most common next world but not necessarily specific to the phrase
+    summarise(tot_freq = sum(freq)) %>%
+    ungroup() %>%
+    arrange(desc(tot_freq))
+  
+  #predicted_word <- options[1,1] %>% pull()
+  
+  return(options)    #return(predicted_word)
+  
+}
--- Saving files.R
+++ Saving files.R
@@ -12,3 +12,37 @@ df <- dataframe <- data.frame(text=unlist(sapply(vec_cor_1000, `[`, "content")),
                               stringsAsFactors=F)
 file_1000_to_use <- df[[1]]
 saveRDS(file_1000_to_use, "./sample_1000_to_use.RDS")
+
+##################### CREATE NGRAMS AND DATATABLE
+
+library(tidyverse)
+library(quanteda)
+library(tm)
+library(data.table)
+library(textclean)
+library(ngram)
+
+sample_file <- readRDS("./DATA/sample_1000_to_use.RDS")
+
+#Generate tokens
+toks <- tokens(sample_file)
+
+#Generate ngrams
+toks_ngram <- tokens_ngrams(toks, n = 2:5, concatenator = " ")
+
+#Create document feature matrix
+doc_feat_matrix <- dfm(toks_ngram)
+
+#Obtain frequencies of each ngram
+ngram_freq <- textstat_frequency(doc_feat_matrix) %>%
+  select(ngram = feature, freq = frequency)
+
+ngram_info <- ngram_freq %>%
+  mutate(pred = word(ngram, -1),
+         ngram_1 = str_replace(ngram, paste0(" ",pred, "$"),"")) %>%
+  select(-ngram)
+
+#Convert to datatable
+ngram_table <- setDT(ngram_info)
+
+head(ngram_table)
